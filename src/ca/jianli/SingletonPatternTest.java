9d407b4685b9fd542675181893008803791ecc13
Jian Li <me@jianli.ca>
2014-03-12 13:58:30 -0700

    Updated Singleton test to compare class instances.

--- src/ca/jianli/SingletonPatternTest.java
+++ src/ca/jianli/SingletonPatternTest.java
@@ -11,14 +11,9 @@ public class SingletonPatternTest {
 	@Test
 	public void test1() {
 		Singleton one = Singleton.getInstance();
-		assertEquals(0, one.get());
-		one.set(100);
-		assertEquals(100, one.get());
-
-		// two is assigned the same Singleton instance as one
-
 		Singleton two = Singleton.getInstance();
-		assertEquals(100, two.get());
+
+		assertEquals(one, two);
 
 		// Below code gives compile error
 		// Singleton three = new Singleton();
