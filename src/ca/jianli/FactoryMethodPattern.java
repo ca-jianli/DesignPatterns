765ffc2d4f2722dd37306db39ffde3e730464ba0
Jian Li <me@jianli.ca>
2014-03-11 23:48:06 -0700

    Added factory method pattern using static method.

---
+++ src/ca/jianli/FactoryMethodPattern.java
@@ -0,0 +1,90 @@
+package ca.jianli;
+
+/**
+ * Factory method defines an interface or high-level logic for constructing an
+ * object, and defers the actual implementation details to subclasses.
+ * 
+ * There two common implementation of factory method.
+ * 
+ * 1) a superclass specifies all standard and generic behavior using virtual
+ * placeholders for creation steps, and then delegate the creation details to
+ * subclasses. This is similar to abstract factory method without the emphasis
+ * on families. This is the original implementation discussed in the Gang of
+ * Four.
+ * 
+ * 2) static method of a class is used to return an object of a type; this
+ * object may be of a sub-class, may be of an existing instance reused, and may
+ * have a more descriptive name.
+ * 
+ * Method 2 is becoming more popular. One reason being that it completely
+ * decouples the client from the implementation details of derived classes.
+ * 
+ * Method 2 is implemented here.
+ * 
+ * @author Jian Li
+ * 
+ */
+public class FactoryMethodPattern {
+
+	// Factory class with static factory method create();
+	public static class FruitFactory {
+		public static Fruit create(int arg) {
+			switch (arg) {
+			case 0:
+				return new Apple();
+			case 1:
+				return new Banana();
+			case 2:
+				return new Strawberry();
+			}
+			return null;
+		}
+	}
+
+	// Fruit interface and concrete fruit classes with private constructor
+
+	public interface Fruit {
+		public String getColor();
+
+		public String getName();
+	}
+
+	public static class Apple implements Fruit {
+		private Apple() {
+		}
+
+		public String getColor() {
+			return "red";
+		}
+
+		public String getName() {
+			return "apple";
+		}
+	}
+
+	public static class Strawberry implements Fruit {
+		private Strawberry() {
+		}
+
+		public String getColor() {
+			return "red";
+		}
+
+		public String getName() {
+			return "strawberry";
+		}
+	}
+
+	public static class Banana implements Fruit {
+		private Banana() {
+		}
+
+		public String getColor() {
+			return "yellow";
+		}
+
+		public String getName() {
+			return "banana";
+		}
+	}
+}
---
+++ src/ca/jianli/FactoryMethodPatternTest.java
@@ -0,0 +1,34 @@
+package ca.jianli;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ca.jianli.FactoryMethodPattern.Fruit;
+import ca.jianli.FactoryMethodPattern.FruitFactory;
+
+public class FactoryMethodPatternTest {
+
+	@Test
+	public void test1() {
+		for (int i = 0; i < 3; i++) {
+			Fruit f = FruitFactory.create(i);
+			String name = f.getName();
+			switch (name) {
+			case "apple":
+				assertEquals("red", f.getColor());
+				break;
+			case "banana":
+				assertEquals("yellow", f.getColor());
+				break;
+			case "strawberry":
+				assertEquals("red", f.getColor());
+				break;
+			default:
+			}
+
+			// Error below because constructor for Apple is private
+			// Fruit f = new FactoryMethodPattern.Apple();
+		}
+	}
+}
