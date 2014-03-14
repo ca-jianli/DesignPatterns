b76190bc04161183bdb979fde87a709481a83426
Jian Li <me@jianli.ca>
2014-03-13 20:52:06 -0700

    Added Bridge Pattern.

---
+++ src/ca/jianli/BridgePattern.java
@@ -0,0 +1,114 @@
+package ca.jianli;
+
+/**
+ * Bridge pattern separates the implementation from the abstraction so the two
+ * can vary independently. Insulation. Expose an interface to implementation in
+ * the class abstraction, but buries concrete implementation in its own
+ * hierarchy.
+ * 
+ * Separate the component into abstraction and implemention. Implementations
+ * become interchangeable at time of component instantiation.
+ * 
+ * Abstraction hiearchy of the component defined as a wrapper to the
+ * implementation hierarchy.
+ * 
+ * Client chooses the wrapper object, and may substitute the wrappee object.
+ * 
+ * As the client makes request, the abstraction objects simply delegates to the
+ * implementation object.
+ * 
+ * @author Jian Li
+ * 
+ */
+
+public class BridgePattern {
+
+	public interface DrawingAPI {
+		public String drawCircle(double x, double y, double radius);
+
+		public String drawSquare(double x, double y, double length);
+	}
+
+	public static class DrawingAPI1 implements DrawingAPI {
+		public String drawCircle(double x, double y, double radius) {
+			return String.format("API1 circle at %f, %f with radius %f", x, y,
+					radius);
+		}
+
+		@Override
+		public String drawSquare(double x, double y, double length) {
+			return String.format("API1 square at %f, %f with length %f", x, y,
+					length);
+		}
+	}
+
+	public static class DrawingAPI2 implements DrawingAPI {
+		public String drawCircle(double x, double y, double radius) {
+			return String.format("API2 circle at %f, %f with radius %f", x, y,
+					radius);
+		}
+
+		@Override
+		public String drawSquare(double x, double y, double length) {
+			return String.format("API2 square at %f, %f with length %f", x, y,
+					length);
+		}
+	}
+
+	public static abstract class Shape {
+		protected DrawingAPI drawingAPI;
+
+		protected Shape(DrawingAPI drawingAPI) {
+			this.drawingAPI = drawingAPI;
+		}
+
+		public abstract String draw();
+
+		public abstract void resizeByPercentage(double pct);
+
+	}
+
+	public static class CircleShape extends Shape {
+		public double x, y, radius;
+
+		protected CircleShape(double x, double y, double radius,
+				DrawingAPI drawingAPI) {
+			super(drawingAPI);
+			this.x = x;
+			this.y = y;
+			this.radius = radius;
+		}
+
+		@Override
+		public String draw() {
+			return drawingAPI.drawCircle(x, y, radius);
+		}
+
+		@Override
+		public void resizeByPercentage(double pct) {
+			radius *= pct;
+		}
+	}
+
+	public static class SquareShape extends Shape {
+		private double x, y, length;
+
+		protected SquareShape(double x, double y, double length,
+				DrawingAPI drawingAPI) {
+			super(drawingAPI);
+			this.x = x;
+			this.y = y;
+			this.length = length;
+		}
+
+		@Override
+		public String draw() {
+			return drawingAPI.drawSquare(x, y, length);
+		}
+
+		@Override
+		public void resizeByPercentage(double pct) {
+			length *= pct;
+		}
+	}
+}
---
+++ src/ca/jianli/BridgePatternTest.java
@@ -0,0 +1,29 @@
+package ca.jianli;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ca.jianli.BridgePattern.CircleShape;
+import ca.jianli.BridgePattern.DrawingAPI1;
+import ca.jianli.BridgePattern.DrawingAPI2;
+import ca.jianli.BridgePattern.SquareShape;
+
+public class BridgePatternTest {
+
+	@Test
+	public void test1() {
+		SquareShape square1 = new SquareShape(1, 1, 2, new DrawingAPI1());
+		SquareShape square2 = new SquareShape(2, 2, 1, new DrawingAPI2());
+		assertEquals("API1 square at 1.000000, 1.000000 with length 2.000000",
+				square1.draw());
+
+		assertEquals("API2 square at 2.000000, 2.000000 with length 1.000000",
+				square2.draw());
+		square2.resizeByPercentage(0.9);
+		square2.draw();
+		CircleShape circle1 = new CircleShape(0, 0, 1, new DrawingAPI1());
+		assertEquals("API1 circle at 0.000000, 0.000000 with radius 1.000000",
+				circle1.draw());
+	}
+}
