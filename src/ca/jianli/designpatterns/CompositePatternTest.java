ef1a9d0828df25314c67a308981aedd6deb72081
Jian Li <me@jianli.ca>
2014-03-18 18:00:25 -0700

    Updated CompositePatternTest

--- src/ca/jianli/designpatterns/CompositePatternTest.java
+++ src/ca/jianli/designpatterns/CompositePatternTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import ca.jianli.designpatterns.CompositePattern.Employee;
 import ca.jianli.designpatterns.CompositePattern.IndividualContributor;
 import ca.jianli.designpatterns.CompositePattern.Manager;
 
@@ -12,14 +11,15 @@ public class CompositePatternTest {
 
 	@Test
 	public void test1() {
-		Employee m1 = new Manager("Tom");
-		Employee i1 = new IndividualContributor("Dick");
-		Employee m2 = new Manager("Harry");
-		Employee i2 = new IndividualContributor("Joe");
-		((Manager) m1).add(i1);
-		((Manager) m1).add(m2);
-		((Manager) m2).add(i2);
+		Manager m1 = new Manager("Tom");
+		IndividualContributor i1 = new IndividualContributor("Dick");
+		Manager m2 = new Manager("Harry");
+		IndividualContributor i2 = new IndividualContributor("Joe");
+		m1.add(i1);
+		m1.add(m2);
+		m2.add(i2);
 		// m1.list();
 		assertEquals(4, m1.count());
+		assertEquals(2, m2.count());
 	}
 }
